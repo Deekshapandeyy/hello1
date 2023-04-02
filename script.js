5586b2a82f9b241bf5bce5cff91690133edff378
DEEKSHA PANDEY <100463731+Deekshapandeyy@users.noreply.github.com>
2023-04-02 20:40:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,66 +1,122 @@
-// function to check recommended weight ,over weight,under weight
-function checkWeight() {
-  var age = document.getElementById("age").value; //get age from user.
-  var weight = document.getElementById("weight").value; //get weight from user.
-  if (age >= 5 && age <= 7 && weight >= 15 && weight <= 20) {
-    // check condition
-    document.getElementById("p").innerHTML = "recommended weight"; //print message
-  } else {
-    if (age >= 5 && age <= 7 && weight < 15) {
-      document.getElementById("p").innerHTML = " You are under weight"; //print message
-    } else {
-      if (age >= 5 && age <= 7 && weight > 15) {
-        document.getElementById("p").innerHTML = " You are over weight"; //print message
-      } else {
-        if (age >= 8 && age <= 10 && weight >= 21 && weight <= 25) {
-          document.getElementById("p").innerHTML = "recommended weight"; //print message
-        } else {
-          if (age >= 8 && age <= 10 && weight < 21) {
-            document.getElementById("p").innerHTML = " You are under weight"; //print message
-          } else {
-            if (age >= 8 && age <= 10 && weight > 25) {
-              document.getElementById("p").innerHTML = " You are over weight"; //print message
-            } else {
-              if (age >= 11 && age <= 15 && weight >= 26 && weight <= 30) {
-                document.getElementById("p").innerHTML = "recommended weight"; //print message
-              } else {
-                if (age >= 11 && age <= 15 && weight < 26) {
-                  document.getElementById("p").innerHTML =
-                    " You are under weight"; //print message
-                } else {
-                  if (age >= 11 && age <= 15 && weight > 26) {
-                    document.getElementById("p").innerHTML =
-                      " You are over weight"; //print message
-                  } else {
-                    if (
-                      age >= 16 &&
-                      age <= 20 &&
-                      weight >= 31 &&
-                      weight <= 40
-                    ) {
-                      document.getElementById("p").innerHTML =
-                        "recommended weight"; //print message
-                    } else {
-                      if (age >= 16 && age <= 20 && weight < 31) {
-                        document.getElementById("p").innerHTML =
-                          " You are under weight"; //print message
-                      } else {
-                        if (age >= 16 && age <= 20 && weight > 31) {
-                          document.getElementById("p").innerHTML =
-                            " You are over weight"; //print message
-                        } else {
-                          document.getElementById("p").innerHTML =
-                            "Enter valid age to check"; //print message
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }
+
+let taskInput=document.getElementById("new-task");
+let addButton=document.getElementsByTagName("button")[0];
+let incompleteTaskHolder=document.getElementById("incomplete-tasks");
+let completedTasksHolder=document.getElementById("completed-tasks");
+
+// creates a new task in string format
+let createNewTaskElement=function(taskString){
+	let listItem=document.createElement("li");
+	let checkBox=document.createElement("input");
+	let label=document.createElement("label");
+	let editInput=document.createElement("input");
+	let editButton=document.createElement("button");
+	let deleteButton=document.createElement("button");
+	label.innerText=taskString;
+	checkBox.type="checkbox";
+	editInput.type="text";
+	editButton.innerText="Edit";
+	editButton.className="edit";
+	editButton.innerText="Edit";
+	editButton.className="edit";
+	deleteButton.innerText="Delete";
+	deleteButton.className="delete";
+	listItem.appendChild(checkBox);
+	listItem.appendChild(label);
+	listItem.appendChild(editInput);
+	listItem.appendChild(editButton);
+	listItem.appendChild(deleteButton);
+	return listItem;
 }
+
+// function to add task in todo
+
+let addTask=function(){
+	  listItem=createNewTaskElement(taskInput.value);
+	  if(taskInput.value.length==0)
+	  {
+		document.getElementById("error").innerHTML="Can not empty todo";
+		
+		return;
+	  }
+
+	  else
+	  {
+		document.getElementById("error").innerHTML="";
+		incompleteTaskHolder.appendChild(listItem);
+	bindTask(listItem, taskCompleted);
+	taskInput.value="";
+	  }
+	
+	
+}
+
+//function to edit task 
+
+let editTask=function(){
+ listItem=this.parentNode;
+//  addButton.setAttribute("value","UPDATE");
+editInput=listItem.querySelector('input[type=text]');
+label=listItem.querySelector("label");
+ containsClass=listItem.classList.contains("editMode");
+
+	
+		if(containsClass){
+			label.innerText=editInput.value;
+		}else{
+			editInput.value=label.innerText;
+		}
+
+		listItem.classList.toggle("editMode");
+}
+
+
+
+
+//Delete task.
+let deleteTask=function(){
+		 listItem=this.parentNode;
+		 ul=listItem.parentNode;
+		ul.removeChild(listItem);
+
+}
+
+
+//Mark task completed
+let taskCompleted=function(){
+	 listItem=this.parentNode;
+	completedTasksHolder.appendChild(listItem);
+				bindTask(listItem, taskIncomplete);
+
+}
+
+// incompleted task
+let taskIncomplete=function(){
+	 listItem=this.parentNode;
+	incompleteTaskHolder.appendChild(listItem);
+	bindTask(listItem,taskCompleted);
+}
+
+//button addition in todo in javascript
+addButton.onclick=addTask;
+
+//binding of task with mousechange events
+let bindTask=function(taskListItem,checkBoxEvent){
+	 checkBox=taskListItem.querySelector("input[type=checkbox]");
+	 editButton=taskListItem.querySelector("button.edit");
+	 deleteButton=taskListItem.querySelector("button.delete");
+		editButton.onclick=editTask;
+		deleteButton.onclick=deleteTask;
+		checkBox.onchange=checkBoxEvent;
+}
+
+//traverse in completed list
+
+	for (var i=0; i<incompleteTaskHolder.children.length;i++){
+		console.log(incompleteTaskHolder.children[i].value);
+		bindTask(incompleteTaskHolder.children[i],taskCompleted);
+	}
+	//bind lists incomplete task
+	for (var i=0; i<completedTasksHolder.children.length;i++){
+		bindTask(completedTasksHolder.children[i],taskIncomplete);
+	}
